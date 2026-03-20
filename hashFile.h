1d26f4601a8b5084318d9b3ada5e029069c46f4a
Vitor Morimitsu <morimitsuv@gmail.com>
2026-03-20 11:02:38 -0300

    fim do .h

--- hashFile.h
+++ hashFile.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /*
     Arquivo .h destinado à implementação e uso de um hash file que contém baldes associados aos endereços binários.
@@ -16,7 +17,15 @@ HashFile criarHashFile();
 
 /// @brief Insere um dado com base no endereço de memória dele
 /// @param hashFile Hash já existente
-void inserirDadoHashFile(HashFile hashFile);
+void inserirDadoHashFile(HashFile hashFile, char* palavra);
+
+/// @brief Remove algum dado de uma hash file
+/// @param hashFile HashFile pré existente
+void removerDadosHashFile(HashFile hashFile, char* palavra);
+
+/// @brief Fecha uma hash file existente
+/// @param hashFile Hash file pré existente
+void fecharHashFile(HashFile hashFile);
 
 /// @brief Organiza os dados da hashfile
 /// @param hashFile Hash já existente
@@ -26,7 +35,7 @@ HashFile organizarDadosHashFile(HashFile hashFile);
 /// @brief Retorna o endereço que determinado dado está no diretório
 /// @param hash Hash já existente
 /// @param palavra Palavra que será procurada
-/// @return 
+/// @return O endereço do dado
 int getEnderecoDiretorioHashFile(HashFile hash, char* palavra);
 
 
